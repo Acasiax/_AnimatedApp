274588fa8a40979585a5e856737a1b9601d99522
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-02-19 00:43:51 +0900

    Initial Commit

---
+++ _AnimatedApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  _AnimatedApp
+//
+//  Created by 이윤지 on 2/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ _AnimatedApp/_AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _AnimatedAppApp.swift
+//  _AnimatedApp
+//
+//  Created by 이윤지 on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct _AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
